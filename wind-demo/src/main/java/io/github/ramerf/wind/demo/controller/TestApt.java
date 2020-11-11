53d79e63719ac18cc23b1cc6ed16af26b0ff35ab
ramer <1390635973@qq.com>
2020-11-11 18:15:41 +0800

    新增：关联查询.

---
+++ wind-demo/src/main/java/io/github/ramerf/wind/demo/controller/TestApt.java
@@ -0,0 +1,15 @@
+package io.github.ramerf.wind.demo.controller;
+
+import io.github.ramerf.wind.core.generator.CommonEnum;
+import java.io.Serializable;
+
+/**
+ * .
+ *
+ * @author Tang Xiaofeng
+ * @since 2020.11.02
+ */
+@CommonEnum(valueType = Double.class)
+public enum TestApt implements Serializable {
+  OHONE(1.2D, "2");
+}
